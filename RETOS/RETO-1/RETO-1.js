152ca4bf52ccc1d6c2cc72762489be3002dfbf25
JoaquinPicazo <joaquin.picazo@alumnos.ulagos.cl>
2023-09-26 15:53:51 -0300

    Avance ejercicios y reto 1

--- RETOS/RETO-1/RETO-1.js
+++ RETOS/RETO-1/RETO-1.js
@@ -1,22 +1,26 @@
-let num = prompt("Ingrese un numero entero para analizar si es par o impar");
+// Se solicita el número al usuario
+let num = prompt("Ingrese un numero ENTERO para analizar si es par o impar");
 
 // Análisis si es decimal o contiene letras en mayúscula o minúscula
 // Si no cumple, se vuelve a solicitar
 let permitido = false;
 while(permitido==false){
+    // Analizar si tiene una letra de la A hasta Z en minuscula o mayuscula
     let mayusculas = /[A-Z]/.test(num);
     let minusculas = /[a-z]/.test(num);
-    // Se analiza que 
+    // Se analiza que no tenga letras mayusculas ni minusculas
     if(mayusculas || minusculas){
         alert("No se permiten letras");
         num = prompt("Ingrese un numero entero para analizar si es par o impar");
         permitido=false;
     } else {
+        // Se analiza que no tenga símbolos
         if(isNaN(num)){
             alert("No se permiten símbolos");
             num = prompt("Ingrese un numero entero para analizar si es par o impar");
             permitido=false;
         }else{
+            // Se analiza que sea un número entero
             if(num-Math.floor(num)==0){
                 permitido=true;         
             }else{
@@ -24,14 +28,15 @@ while(permitido==false){
                 num = prompt("Ingrese un numero entero para analizar si es par o impar");
                 permitido=false;
             }
-        }
-       
+        }  
     }
 }
 
 // Si llega a cumplir los requisitos, permite que se analice si es par o impar y lo muestra en pantalla
 if(num%2==0){
     alert("El numero ingresado es par");
+    document.write("El numero ingresado es par")
 }else{
+    document.write("El numero ingresado es impar")
     alert("El numero ingresado es impar")
 }
